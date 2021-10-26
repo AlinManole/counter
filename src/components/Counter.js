50be0b87dee1f7b34cbd8596c161d18a4c2ac0a1
Alin Manole <alin.manole@yahoo.com>
2021-10-26 17:07:09 +0200

    update

---
+++ src/components/Counter.js
@@ -0,0 +1,29 @@
+import React from "react";
+
+class Counter extends React.Component {
+
+    //   constructor() {
+    //     super()
+
+    //   }
+    render() {
+
+        return (
+            <div style={{ display: "flex", alignItems: "center", justifyContent: "center", flexDirection: "column" }}>
+                <div className="container" style={{ padding: "50px", margin: "10px", display: "flex", alignItems: "center", justifyContent: "center", border: "2px solid black", width: "560px" }}>
+                    <button style={{ background: "white", fontSize: "30px", border: "solid 2px black", padding: "32px 40px", borderRadius: "50%", margin: "50px" }} onClick={this.props.substract}>-</button>
+                    <h2 style={{ padding: "30px" }}>{this.props.count}</h2>
+                    <button style={{ background: "white", fontSize: "30px", border: "solid 2px black", padding: "32px 40px", borderRadius: "50%", margin: "50px" }} onClick={this.props.increment}>+</button>
+                </div>
+
+                <div className="container" style={{ padding: "50px", margin: "10px", display: "flex", alignItems: "center", justifyContent: "center", border: "2px solid black", width: "560px" }}>
+                    <button style={{ background: "white", fontSize: "30px", border: "solid 2px black", padding: "32px 40px", borderRadius: "50%", margin: "50px" }} onClick={this.props.substract2}>-</button>
+                    <h2 style={{ padding: "30px" }}>{this.props.count2}</h2>
+                    <button style={{ background: "white", fontSize: "30px", border: "solid 2px black", padding: "32px 40px", borderRadius: "50%", margin: "50px" }} onClick={this.props.increment2}>+</button>
+                </div>
+            </div>
+        )
+    }
+}
+
+export default Counter;
