7fbcec6e91f12b949c519c0401474c1e5f0506e7
Alin Manole <alin.manole@yahoo.com>
2021-10-26 17:19:09 +0200

    update 5.18

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -2,10 +2,7 @@ import React from "react";
 
 class Counter extends React.Component {
 
-    //   constructor() {
-    //     super()
 
-    //   }
     render() {
 
         return (
@@ -15,7 +12,6 @@ class Counter extends React.Component {
                     <h2 style={{ padding: "30px" }}>{this.props.count}</h2>
                     <button style={{ background: "white", fontSize: "30px", border: "solid 2px black", padding: "32px 40px", borderRadius: "50%", margin: "50px" }} onClick={this.props.increment}>+</button>
                 </div>
-
                 <div className="container" style={{ padding: "50px", margin: "10px", display: "flex", alignItems: "center", justifyContent: "center", border: "2px solid black", width: "560px" }}>
                     <button style={{ background: "white", fontSize: "30px", border: "solid 2px black", padding: "32px 40px", borderRadius: "50%", margin: "50px" }} onClick={this.props.substract2}>-</button>
                     <h2 style={{ padding: "30px" }}>{this.props.count2}</h2>
